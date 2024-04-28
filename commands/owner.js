f53398cc06d380170bcd1f1ef75f50eb0fb8546f
Boss <161413826+itsb0ss@users.noreply.github.com>
2024-04-28 06:38:51 -0700

    Update owner.js 3

--- commands/owner.js
+++ commands/owner.js
@@ -34,7 +34,7 @@ module.exports = {
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157310253520662638/2443-iconperson.png?ex=651824aa&is=6516d32a&hm=0becc4a0fda01e5a02a63cf098db30c287e60a474f8d2da4ddeae7f47d98a5a3&',
           url: 'https://discord.gg/FUEHs7RCqz'
         })
-            .setDescription(`__**About me**__:\n\n I am Boss, a fellow Discord user, who happened to have put this bot up online. : ❤️ [RTX GAMING](${youtubeLink})\n Roblox : 💙 [rtxxgg](${RobloxLink})`)
+            .setDescription(`__**About me**__:\n\n I am Boss, a fellow Discord user, who happened to have put this bot up online. : ❤️ [YouTube](${youtubeLink})\n 💙 [Roblox](${RobloxLink})`)
             .setTimestamp();
       interaction.reply({ embeds: [embed] }).catch(e => {});
 
